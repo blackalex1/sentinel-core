d6ce9c913fe7ed1eb22eeac826f89e64a0b78691
Alex <blackalex1@users.noreply.github.com>
2026-08-27 19:38:05 +0300

    test: sanitize fixture IP addresses to RFC 5737 documentation subnets

--- pkg/parser/subscription_test.go
+++ pkg/parser/subscription_test.go
@@ -10,12 +10,12 @@ import (
 func TestParseSubscription(t *testing.T) {
 	rawSubscription := `
 # Russian Mobile White-List VLESS reality test
-vless://11111111-2222-3333-4444-555555555555@95.217.1.1:443?security=reality&sni=google.com&pbk=abcd1234efgh5678abcd1234efgh5678abcd1234efg&sid=12345678&fp=chrome&type=tcp&flow=xtls-rprx-vision#Finland-Reality
+vless://11111111-2222-3333-4444-555555555555@198.51.100.1:443?security=reality&sni=google.com&pbk=abcd1234efgh5678abcd1234efgh5678abcd1234efg&sid=12345678&fp=chrome&type=tcp&flow=xtls-rprx-vision#Finland-Reality
 
 // Another node
-vless://aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee@185.100.1.1:8443?security=reality&sni=microsoft.com&pbk=1111222233334444555566667777888899990000aaa&sid=87654321&fp=safari&type=grpc&serviceName=vless-grpc#Germany-Reality
+vless://aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee@198.51.100.2:8443?security=reality&sni=microsoft.com&pbk=1111222233334444555566667777888899990000aaa&sid=87654321&fp=safari&type=grpc&serviceName=vless-grpc#Germany-Reality
 
-hy2://mysecretpassword@194.87.1.1:443?sni=apple.com&insecure=1#Hysteria2-Node
+hy2://mysecretpassword@198.51.100.3:443?sni=apple.com&insecure=1#Hysteria2-Node
 `
 
 	profiles, err := ParseSubscription(rawSubscription)
@@ -32,7 +32,7 @@ hy2://mysecretpassword@194.87.1.1:443?sni=apple.com&insecure=1#Hysteria2-Node
 	if p1.Protocol != ast.ProtoVLESS {
 		t.Errorf("expected ProtoVLESS, got %v", p1.Protocol)
 	}
-	if p1.Address != "95.217.1.1" || p1.Port != 443 {
+	if p1.Address != "198.51.100.1" || p1.Port != 443 {
 		t.Errorf("p1 address/port mismatch: %s:%d", p1.Address, p1.Port)
 	}
 	if p1.SNI != "google.com" || p1.Security != "reality" {
